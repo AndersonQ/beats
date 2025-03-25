b3937cabac87cb89bf6073b4f0999aafbf12b379
Anderson Queiroz <anderson.queiroz@elastic.co>
2025-03-25 12:40:02 +0100

    benchmarks

--- filebeat/input/filestream/gzip_test.go
+++ filebeat/input/filestream/gzip_test.go
@@ -1,116 +1,371 @@
 package filestream
 
 import (
-	"bufio"
+	"crypto/rand"
+	"errors"
 	"fmt"
+	"io"
+	"math"
 	"os"
 	"path/filepath"
-	"sync"
 	"testing"
+	"time"
 
+	"github.com/dustin/go-humanize"
 	"github.com/klauspost/compress/gzip"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/stretchr/testify/require"
+
+	"github.com/elastic/beats/v7/libbeat/reader"
+	"github.com/elastic/beats/v7/libbeat/reader/readfile"
+	"github.com/elastic/beats/v7/libbeat/reader/readfile/encoding"
 )
 
-type offsetReader struct {
-	offset  int
-	scanner *bufio.Scanner
-	mx      sync.Mutex
+var lineText func() string
+var sizes = []int{
+	1 * humanize.MiByte,
+	5 * humanize.MiByte,
+	25 * humanize.MiByte,
+	50 * humanize.MiByte,
+	100 * humanize.MiByte,
+	250 * humanize.MiByte,
+	500 * humanize.MiByte,
+	1000 * humanize.MiByte,
 }
 
-func (o *offsetReader) Offset() int {
-	o.mx.Lock()
-	defer o.mx.Unlock()
-	return o.offset
+func BenchmarkGzip(b *testing.B) {
+	publisher := noopPublisher{}
+
+	tcs := []struct {
+		name   string
+		lineFn func() string
+	}{
+		{name: "static-line", lineFn: func() string { return "line" }},
+		{name: "random-line", lineFn: rand.Text},
+	}
+	for _, tc := range tcs {
+		lineText = tc.lineFn
+
+		b.Run(tc.name, func(b *testing.B) {
+			for _, size := range sizes {
+				plainFile, gzFile, lines := createLogFile(
+					b, calculateLines(size))
+
+				half := lines / 2
+				leftOver := half + (lines % 2)
+
+				// b.Run(fmt.Sprintf("%s-plain-file", humanize.Bytes(uint64(size))),
+				// 	func(b *testing.B) {
+				// 		for i := 0; i < b.N; i++ {
+				// 			readPlainFile(b, plainFile, half, leftOver, publisher)
+				// 		}
+				// 	})
+				//
+				// b.Run(fmt.Sprintf("%s-gzip-file", humanize.Bytes(uint64(size))),
+				// 	func(b *testing.B) {
+				// 		for i := 0; i < b.N; i++ {
+				// 			readGZFile(b, gzFile, half, leftOver, publisher)
+				// 		}
+				// 	})
+
+				b.Run(fmt.Sprintf("%s", humanize.Bytes(uint64(size))),
+					func(b *testing.B) {
+						for i := 0; i < b.N; i++ {
+							// readPlainFile(b, plainFile, half, leftOver, publisher)
+							readGZFile(b, gzFile, half, leftOver, publisher)
+						}
+					})
+
+				err := os.Remove(plainFile)
+				assert.NoError(b, err, "could not delete %s file %s", plainFile)
+				err = os.Remove(gzFile)
+				assert.NoError(b, err, "could not delete %s file %s", gzFile)
+			}
+		})
+	}
 }
 
-// Seek sets the offset for the next Read or Write on file to offset, interpreted
-// according to whence: 0 means relative to the origin of the file, 1 means
-// relative to the current offset, and 2 means relative to the end.
-// It returns the new offset and an error, if any.
-// The behavior of Seek on a file opened with O_APPEND is not specified.
+func TestTimeDifferenceGzipPlain(t *testing.T) {
+	publisher := noopPublisher{}
 
-// Seek will read the file until offset,
-func (o *offsetReader) Seek(offset int, whence int) (ret int, err error) {
-	o.mx.Lock()
-	defer o.mx.Unlock()
+	tcs := []struct {
+		name   string
+		lineFn func() string
+	}{
+		{name: "static-line", lineFn: func() string { return "line" }},
+		{name: "random-line", lineFn: rand.Text},
+	}
+	for _, tc := range tcs {
+		lineText = tc.lineFn
+		t.Run(tc.name, func(t *testing.T) {
+			for _, size := range sizes {
 
-	switch whence {
-	case 0:
-		o.offset = offset
-	case 1:
-		o.offset += offset
-	case 2:
-		panic("unimplemented")
-	default:
-		return 0, fmt.Errorf("%d is an invalid whence", whence)
+				plainFile, gzFile, lines := createLogFile(
+					t, calculateLines(size))
+
+				half := lines / 2
+				leftOver := half + (lines % 2)
+
+				var plainDuration, gzipDuration time.Duration
+				t.Run(fmt.Sprintf("%s-plain-file", humanize.Bytes(uint64(size))),
+					func(b *testing.T) {
+						now := time.Now()
+						readPlainFile(b, plainFile, half, leftOver, publisher)
+						plainDuration = time.Since(now)
+						b.Logf("readPlainFile took %s for %d lines", plainDuration, lines)
+					})
+
+				t.Run(fmt.Sprintf("%s-gzip-file", humanize.Bytes(uint64(size))),
+					func(b *testing.T) {
+						now := time.Now()
+						readGZFile(b, gzFile, half, leftOver, publisher)
+						gzipDuration = time.Since(now)
+						b.Logf("readGZFile took %s for %d lines", gzipDuration, lines)
+					})
+
+				t.Logf("\n%s: for %s, gzip read took %s more than pain read\n\n",
+					tc.name,
+					humanize.Bytes(uint64(size)),
+					gzipDuration-plainDuration)
+
+				err := os.Remove(plainFile)
+				assert.NoError(t, err, "could not delete %s file %s", plainFile)
+				err = os.Remove(gzFile)
+				assert.NoError(t, err, "could not delete %s file %s", gzFile)
+			}
+		})
+	}
+}
+
+func TestGzip(t *testing.T) {
+	_, gzFile, lines := createLogFile(t, 42)
+
+	publisher := &debugPublisher{}
+	half := lines / 2
+	leftOver := half + (lines % 2)
+
+	readGZFile(t, gzFile, half, leftOver, publisher)
+}
+
+// ================================ Publishers ================================
+type Publisher interface {
+	Publish([]byte)
+}
+
+type noopPublisher struct{}
+
+func (n noopPublisher) Publish([]byte) {}
+
+type debugPublisher struct {
+	counter int
+}
+
+func (p *debugPublisher) Publish(data []byte) {
+	fmt.Println(string(data))
+	p.counter++
+}
+
+// ============================= gzipSeekerReader =============================
+
+type gzipSeekerReader struct {
+	f   *os.File
+	gzr *gzip.Reader
+}
+
+func (r *gzipSeekerReader) Read(p []byte) (n int, err error) {
+	return r.gzr.Read(p)
+}
+
+func (r *gzipSeekerReader) Close() error {
+	return r.gzr.Close()
+}
+
+func (r *gzipSeekerReader) Seek(offset int64, whence int) (int64, error) {
+	if whence != 0 {
+		return 0, fmt.Errorf("only 0 is allowed for whence, got: %d", whence)
 	}
 
-	for i := range o.offset {
-		if !o.scanner.Scan() {
-			return i, fmt.Errorf("%d is an invalid offset", whence)
+	// Not used in the test/benchmark
+	if offset == 0 {
+		n, err := r.f.Seek(0, 0)
+		if err != nil {
+			return n, fmt.Errorf("gzipSeekerReader: could not seek to 0: %w", err)
+		}
+
+		if err = r.gzr.Close(); err != nil {
+			return n, fmt.Errorf(
+				"gzipSeekerReader: could not close gzip reader before creating a new one: %w", err)
+		}
+		r.gzr, err = gzip.NewReader(r.f)
+		if err != nil {
+			return n, fmt.Errorf("gzipSeekerReader: could not create new gzip reader: %w", err)
+		}
+		return n, nil
+	}
+
+	var n int
+	var err error
+	buffSize := int64(512)
+	if offset <= buffSize {
+		n, err = r.Read(make([]byte, offset))
+		if err != nil {
+			return int64(n), fmt.Errorf("gzipSeekerReader: could read offset=%d: %w", offset, err)
+		}
+		return int64(n), nil
+	}
+
+	chunks := offset / buffSize
+	leftover := offset % buffSize
+	buff := make([]byte, buffSize)
+	read := 0
+	for i := range chunks {
+		n, err = r.gzr.Read(buff)
+		read += n
+		if err != nil {
+			return int64(read), fmt.Errorf("gzipSeekerReader: could read chunk %d: %w", i, err)
+		}
+	}
+
+	if leftover > 0 {
+		n, err = r.Read(make([]byte, leftover))
+		read += n
+		if err != nil && err != io.EOF {
+			return int64(read), fmt.Errorf("gzipSeekerReader: could read leftover %d: %w", leftover, err)
 		}
 	}
 
 	return offset, nil
 }
 
-func (o *offsetReader) Scan() bool {
-	return o.scanner.Scan()
-}
+// ============================= helper functions =============================
 
-func (o *offsetReader) Text() string {
-	o.mx.Lock()
-	defer o.mx.Unlock()
-	o.offset++
-	return o.scanner.Text()
-}
+func readPlainFile(
+	t testing.TB, path string, half, rest int, publisher Publisher) {
 
-func TestOffset(t *testing.T) {
-	path := filepath.Join("testdata", "gzip", "10-logs.log.gz")
-
-	gzFile, err := os.Open(path)
+	f1, err := os.Open(path)
 	require.NoErrorf(t, err, "could not open log file %s", path)
-	defer gzFile.Close()
+	defer f1.Close()
 
-	gzr, err := gzip.NewReader(gzFile)
+	f2, err := os.Open(path)
+	require.NoErrorf(t, err, "could not open log file %s", path)
+	defer f2.Close()
+
+	readInHalfThenHalf(t, half, rest, f1, f2, publisher)
+}
+
+func readGZFile(
+	t testing.TB, path string, half, rest int, publisher Publisher) {
+
+	gzSeekReader1, f1, gzr1 := newGZSeekReader(t, path)
+	defer gzr1.Close()
+	defer f1.Close()
+
+	gzSeekReader2, f2, gzr2 := newGZSeekReader(t, path)
+	defer gzr2.Close()
+	defer f2.Close()
+
+	readInHalfThenHalf(t, half, rest, gzSeekReader1, gzSeekReader2, publisher)
+}
+
+func newGZSeekReader(t testing.TB, path string) (*gzipSeekerReader, *os.File, *gzip.Reader) {
+	f, err := os.Open(path)
+	require.NoErrorf(t, err, "could not open log file %s", path)
+
+	gzr, err := gzip.NewReader(f)
 	require.NoErrorf(t, err, "could not create gzip reader")
+	gzSeekReader := &gzipSeekerReader{f: f, gzr: gzr}
 
+	return gzSeekReader, f, gzr
+}
+
+func readInHalfThenHalf(
+	t testing.TB,
+	half int,
+	rest int,
+	reader1 io.ReadSeekCloser,
+	reader2 io.ReadSeekCloser,
+	publisher Publisher) {
+
+	offset, err := readNLines(t, reader1, half, publisher)
+
+	_, err = reader2.Seek(int64(offset), 0)
+	require.NoErrorf(t, err, "could not seek")
+
+	offset, err = readNLines(t, reader2, rest, publisher)
+	require.NoError(t, err)
+}
+
+func readNLines(t testing.TB, f io.ReadCloser, n int, publisher Publisher) (int, error) {
+	codec, err := encoding.Plain(f)
+	require.NoError(t, err, "could not create encoder")
+
+	var r reader.Reader
+	r, err = readfile.NewEncodeReader(f, readfile.Config{
+		Codec:      codec,
+		BufferSize: 4096,
+		Terminator: readfile.AutoLineTerminator,
+	})
+	require.NoError(t, err)
+	r = readfile.NewStripNewline(r, readfile.AutoLineTerminator)
+
+	var m reader.Message
 	offset := 0
-	offreader := &offsetReader{scanner: bufio.NewScanner(gzr)}
-
-	fmt.Printf("reading 1st 5 lines\n\n")
-
-	for range 5 {
-		offreader.Scan()
-		line := offreader.Text()
-		fmt.Printf("\t%s\n", line)
-		offset++
+	linesRead := 0
+	for range n {
+		m, err = r.Next()
+		if err != nil {
+			break
+		}
+		offset += m.Offset + m.Bytes
+		linesRead++
+		publisher.Publish(m.Content)
 	}
-	fmt.Printf("offset: %d\n", offset)
-	fmt.Printf("offreader.Offset(): %d\n", offreader.Offset())
-	fmt.Printf("\nread 1st 5 lines\n")
 
-	gzFile2, err := os.Open(path)
-	require.NoErrorf(t, err, "could not open log file %s", path)
-	defer gzFile2.Close()
-
-	gzr2, err := gzip.NewReader(gzFile2)
-	require.NoErrorf(t, err, "could not create gzip reader")
-
-	offset2 := 0
-	offreader2 := &offsetReader{scanner: bufio.NewScanner(gzr2)}
-	ret, err := offreader2.Seek(offset, 0)
-	assert.Equalf(t, offset, ret, "unexpected seek advance. Got %d, want %d",
-		ret, offset)
-	for range 5 {
-		offreader2.Scan()
-		line := offreader2.Text()
-		fmt.Printf("\t%s\n", line)
-		offset2++
+	if linesRead != n {
+		err = errors.Join(err, fmt.Errorf("expected %d lines read, got %d",
+			n, offset))
 	}
-	fmt.Printf("offset2: %d\n", offset2)
-	fmt.Printf("offreader2.Offset(): %d\n", offreader2.Offset())
+	return offset, err
+}
+
+// calculateLines calculates the approximate number of lines to reach the
+// size in bytes. Tip, use humanize.KiByte, humanize.MiByte, and so on.
+func calculateLines(sizeInBytes int) int {
+	text := []byte(lineText() + " xxxxxxxxx")
+	return int(math.Ceil(float64(sizeInBytes) / float64(len(text))))
+}
+
+func createLogFile(t testing.TB, lines int) (string, string, int) {
+	filename := filepath.Join(t.TempDir(), "log.log")
+	gzFilename := filename + ".gz"
+
+	f, err := os.Create(filename)
+	require.NoErrorf(t, err, "could not create log file")
+
+	for i := range lines {
+		// _, err = fmt.Fprintln(f, "line "+strconv.Itoa(i))
+		_, err = fmt.Fprintf(f, "line %d - %s\n", i, lineText())
+		require.NoErrorf(t, err, "could not write line %d/%d to log file",
+			i, lines)
+	}
+	require.NoErrorf(t, f.Close(), "could not close log file")
+
+	plainFile, err := os.Open(filename)
+	stat, err := plainFile.Stat()
+	require.NoErrorf(t, err, "could not stat log file")
+	t.Logf("plain file to be gzipped has %s", humanize.Bytes(uint64(stat.Size())))
+
+	gzipFile, err := os.Create(gzFilename)
+	require.NoErrorf(t, err, "could not open log file")
+
+	gzw := gzip.NewWriter(gzipFile)
+
+	_, err = io.Copy(gzw, plainFile)
+	require.NoErrorf(t, err, "could write date to gzip file")
+	require.NoErrorf(t, gzw.Close(), "could not close gzip writer")
+	require.NoErrorf(t, gzipFile.Close(), "could not close gzip file")
+	assert.NoErrorf(t, plainFile.Close(), "could not close plain file used of for reading")
+
+	return filename, gzFilename, lines
 }
