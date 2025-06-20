455903c125742dd5069d5b85dc50ff6a6f1866e7
Anderson Queiroz <anderson.queiroz@elastic.co>
2025-06-20 12:00:20 +0200

    add GZIP to file FileDescriptor and detect on fswatch

--- filebeat/input/filestream/fswatch_test.go
+++ filebeat/input/filestream/fswatch_test.go
@@ -1052,6 +1052,7 @@ func filenames(m map[string]loginp.FileDescriptor) (result string) {
 	return result
 }
 
+// TODO(anderson): check benchmark
 func BenchmarkToFileDescriptor(b *testing.B) {
 	dir := b.TempDir()
 	basename := "created.log"
