5a0f6415e15c7d62d1203ea2e602ced502868ca1
Anderson Queiroz <anderson.queiroz@elastic.co>
2026-03-19 09:16:38 +0100

    fix after cherry-pick

--- filebeat/input/filestream/logger.go
+++ filebeat/input/filestream/logger.go
@@ -18,9 +18,6 @@
 package filestream
 
 import (
-	"crypto/sha256"
-	"encoding/hex"
-
 	loginp "github.com/elastic/beats/v7/filebeat/input/filestream/internal/input-logfile"
 	"github.com/elastic/elastic-agent-libs/logp"
 )
